159d5667e1e5acf2686ee09900505699c70c2681
Yeimer Molina <yeimer.molina@gmail.com>
2019-01-23 20:38:34 -0500

    Add pan responder, move container

--- src/Deck.js
+++ src/Deck.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { 
+  View,
+  Animated, 
+  PanResponder 
+} from 'react-native';
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {
+        position.setValue({ x: 0, y: 0 });
+      }
+    });
+
+    this.state = { panResponder, position };
+
+  }
   renderCards() {
     return this.props.data.map(item => (
       this.props.renderCard(item)
@@ -10,9 +32,12 @@ class Deck extends Component {
 
   render() {
     return (
-      <View>
+      <Animated.View 
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}
+      >
         {this.renderCards()}
-      </View>
+      </Animated.View>
     );
   }
 }
