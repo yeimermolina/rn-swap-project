d9bd9161f3d9ae3161cc528bbc0808122b3e19c5
Yeimer Molina <yeimer.molina@gmail.com>
2019-01-25 18:23:50 -0500

    Rotate card

--- src/Deck.js
+++ src/Deck.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import { 
   View,
   Animated, 
-  PanResponder 
+  PanResponder,
+  Dimensions
 } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
   constructor(props) {
@@ -24,12 +27,26 @@ class Deck extends Component {
     this.state = { panResponder, position };
 
   }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
+    });
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
   renderCards() {
     return this.props.data.map((item, index) => {
       if(index === 0) {
         return (
-          <Animated.View 
-            style={this.state.position.getLayout()}
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
             {...this.state.panResponder.panHandlers}
           >
             {this.props.renderCard(item)}
